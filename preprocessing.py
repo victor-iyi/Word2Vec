dfe8c9d3f613592083f60821d4b181c709c90908
victor-iyiola <javafolabi@gmail.com>
2017-10-28 16:06:32 +0100

    Added preprocessing.py

---
+++ preprocessing.py
@@ -0,0 +1,16 @@
+"""
+  @author Victor I. Afolabi
+  A.I. Engineer & Software developer
+  javafolabi@gmail.com
+  
+  Created on 28 October, 2017 @ 3:56 PM.
+  
+  Copyright © 2017. Victor. All rights reserved.
+"""
+
+from nltk import word_tokenize, sent_tokenize
+
+
+class WordFeatures():
+    def __init__(self, file):
+        
